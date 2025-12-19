4a707ffb4705a01165398b896aa6ab520e0967c5
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-12-19 21:08:27 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,8 +1,1 @@
- import steamlit as st
- import os 
- import pickle
- import pandas as pd 
-  from sklearn.preprocessing ds import 
-  import sklearn as sk 
 
-  model_path = os.path.join('model'.pl)
